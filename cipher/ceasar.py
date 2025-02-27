e9f67dbcac535616962fe2da25c8c45066241a74
David Osborne <156497440+DavidJOsborne@users.noreply.github.com>
2025-02-27 09:10:41 -0600

    Update ceasar.py

--- cipher/ceasar.py
+++ cipher/ceasar.py
@@ -4,24 +4,33 @@ while True:
   option = raw_input("Choose an option\nencrypt (type 1)\ndecrypt with key (type 2)\ndecrypt with brute force (type 3)")
   if option == "1":
     character = raw_input("what would you like to encrypt?")
-    key = raw_input("what is the key?")
+    key = int(raw_input("what is the key?"))
     for i in character:
-      newWord += alphabet[(alphabet.find(i) + int(key)) % 26]
+      if i.lower() in alphabet:
+        newWord += alphabet[(alphabet.find(i.lower()) + key) % 26]
+      else:
+        newWord += i
     print newWord
   elif option == "2":
     character = raw_input("what would you like to decrypt?")
-    key = raw_input("what is the key?")
+    key = int(raw_input("what is the key?"))
     for i in character:
-      newWord += alphabet[(alphabet.find(i) - int(key)) % 26]
+      if i.lower() in alphabet:
+        newWord += alphabet[(alphabet.find(i.lower()) - key) % 26]
+      else:
+        newWord += i
     print newWord
   elif option == "3":
     character = raw_input("what would you like to decrypt?")
-    l = 1
-    while l < 26:
+    key = 1
+    while key < 26:
       newWord = ""
       for i in character:
-        newWord += alphabet[(alphabet.find(i) - int(l)) % 26]
-      print newWord
-      l+=1
+        if i.lower() in alphabet:
+          newWord += alphabet[(alphabet.find(i.lower()) - key) % 26]
+        else:
+          newWord += i
+      print key,":",newWord
+      key+=1
   else:
     print("invalid input")
